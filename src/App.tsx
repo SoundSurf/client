c40909b8a60195ee2e8f92a79b511a0531532fc2
sh981013s <sh981013s@gmail.com>
2024-06-08 01:40:37 +0900

    feat: Add AuthTopNav component to parent Route element in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import SignupPage from "@/components/pages/signupPage/SignupPage.tsx";
 import ROUTES from "@/constants/routes.ts";
 import GlobalStyle from "@/styles/GlobalStyle.tsx";
 import theme from "@/styles/theme.ts";
+import AuthTopNav from "@/components/_common/authTopNav/AuthTopNav.tsx";
 
 const App = () => {
   return (
@@ -19,8 +20,10 @@ const App = () => {
           <Suspense fallback={<div>loading....</div>}>
             <Routes>
               <Route path={ROUTES.home} element={<MainPage />} />
-              <Route path={ROUTES.login} element={<LoginPage />} />
-              <Route path={ROUTES.signup} element={<SignupPage />} />
+              <Route path="/" element={<AuthTopNav />}>
+                <Route path={ROUTES.login} element={<LoginPage />} />
+                <Route path={ROUTES.signup} element={<SignupPage />} />
+              </Route>
             </Routes>
           </Suspense>
         </TopLayout>
