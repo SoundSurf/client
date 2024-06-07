70d0df12e94b65bea3d239e66fc9f599f51688e1
sh981013s <sh981013s@gmail.com>
2024-06-08 01:41:57 +0900

    feat: add auth top nav using outlet

---
+++ src/components/authTopNav/AuthTopNav.tsx
@@ -0,0 +1,12 @@
+import { Outlet } from "react-router-dom";
+
+const AuthTopNav = () => {
+  return (
+    <>
+      <div>auth top nav</div>
+      <Outlet />
+    </>
+  );
+};
+
+export default AuthTopNav;
