83ea9918952b531cd3fe5ce18cc38666b0732fdb
sh981013s <sh981013s@gmail.com>
2024-06-08 17:56:38 +0900

    refactor: USER_REGEX PASSWORD validation regex pattern to include specific criteria

--- src/constants/regex.ts
+++ src/constants/regex.ts
@@ -1,4 +1,4 @@
 export const USER_REGEX = {
   EMAIL: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,10}$/,
-  PASSWORD: /^.{1,}$/,
+  PASSWORD: /^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])[A-Za-z0-9]{6,16}$/,
 };
