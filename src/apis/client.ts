ffb490a81ed63819bd0c2b8bd20d98e54b547a01
sh981013s <sh981013s@gmail.com>
2024-06-08 23:16:51 +0900

    refactor: add authorization token interceptor in client.ts

--- src/apis/client.ts
+++ src/apis/client.ts
@@ -6,4 +6,17 @@ const clientInstance = axios.create({
   baseURL: SERVER_BASE_URL,
 });
 
+clientInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("userToken");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
+
 export default clientInstance;
