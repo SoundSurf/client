42cca5b79c998e1ea26bc85473165aaff27ca012
sh981013s <sh981013s@gmail.com>
2024-06-10 21:46:12 +0900

    feat: add basic drive nav component

---
+++ src/components/_common/driveNav/DriveNav.tsx
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+import HamburgerBtn from "@/components/_common/hamburgerBtn/HamburgerBtn.tsx";
+import Search from "@/assets/icons/search.svg?react";
+
+const DriveNavWrapper = styled.nav`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 1.5rem 0 1.5rem;
+`;
+
+const NavRight = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 20%;
+`;
+
+const DriveNav = () => {
+  return (
+    <DriveNavWrapper>
+      <div>logo</div>
+      <NavRight>
+        <Search />
+        <HamburgerBtn />
+      </NavRight>
+    </DriveNavWrapper>
+  );
+};
+
+export default DriveNav;
