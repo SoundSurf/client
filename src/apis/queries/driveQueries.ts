b36b015e6bbab14d84ee8f2416361a74d440e2ad
sh981013s <sh981013s@gmail.com>
2024-06-12 03:42:55 +0900

    feat: update useRecommendation query function to have functionality to fetch prev, next data

--- src/apis/queries/driveQueries.ts
+++ src/apis/queries/driveQueries.ts
@@ -1,13 +1,44 @@
-import { useSuspenseQuery } from "@tanstack/react-query";
-import { getRecommendations } from "@/apis/driveApis/driveApis.ts";
+import { useState, useEffect } from "react";
+import { useQuery, useMutation } from "@tanstack/react-query";
+import {
+  getRecommendations,
+  getPrevTracks,
+  getNextTracks,
+} from "@/apis/driveApis/driveApis.ts";
+import { RecommendationRes } from "@/ssTypes/drive/driveTypes.ts";
 
 export const useRecommendations = (genreId: number | "all") => {
-  const { data: recommendations } = useSuspenseQuery({
+  const { data: initialRecommendations } = useQuery({
     queryKey: ["recommendations", genreId],
     queryFn: () => getRecommendations(genreId),
   });
 
+  const [recommendations, setRecommendations] =
+    useState<RecommendationRes | null>(null);
+
+  useEffect(() => {
+    if (initialRecommendations) {
+      setRecommendations(initialRecommendations);
+    }
+  }, [initialRecommendations]);
+
+  const prevTracksMutation = useMutation({
+    mutationFn: () => getPrevTracks(genreId),
+    onSuccess: (data) => {
+      setRecommendations(data);
+    },
+  });
+
+  const nextTracksMutation = useMutation({
+    mutationFn: () => getNextTracks(genreId),
+    onSuccess: (data) => {
+      setRecommendations(data);
+    },
+  });
+
   return {
     recommendations,
+    getPrevTracks: prevTracksMutation.mutate,
+    getNextTracks: nextTracksMutation.mutate,
   };
 };
