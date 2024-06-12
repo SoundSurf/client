e7b22dee745ea61fa20a8eafb3716b3ef2ce72d2
sh981013s <sh981013s@gmail.com>
2024-06-12 17:09:03 +0900

    refactor: refactor driveTypes.ts for better organization and readability.

--- src/ssTypes/drive/driveTypes.ts
+++ src/ssTypes/drive/driveTypes.ts
@@ -37,3 +37,42 @@ export type RecommendationRes = {
   nowSong: Song;
   nextSong: Song;
 };
+
+export type AlbumSimple = {
+  albumName: string;
+  id: string;
+  releaseDate: string;
+  spotifyUrl: string;
+  genres: string;
+  rating: string;
+  albumType: string;
+  artists: Artist[];
+  images: string[];
+};
+
+export type SongInfo = {
+  id: string;
+  name: string;
+  previewUrl: string;
+  spotifyUrl: string;
+  durationMs: number;
+  album: AlbumSimple;
+  artists: Artist[];
+};
+
+export type RelatedSong = {
+  id: string;
+  name: string;
+  albumId: string;
+  previewUrl: string;
+  artists: Artist[];
+  images: string[];
+};
+
+export type AlbumInfoResponse = {
+  album: {
+    albumSimple: AlbumSimple;
+    songs: SongInfo[];
+  };
+  relatedSongs: RelatedSong[];
+};
